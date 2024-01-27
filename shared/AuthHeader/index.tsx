e7a1b79d62f533c3700487c514aa510b074242bf
vineetkbaghel <vineet.tecmantras@gmail.com>
2024-01-27 17:49:15 +0530

    worked on the global search functionality

--- shared/AuthHeader/index.tsx
+++ shared/AuthHeader/index.tsx
@@ -2,17 +2,14 @@
 /* eslint-disable quote-props */
 /* eslint-disable quotes */
 import * as React from 'react'
-import {styled, alpha} from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import InputBase from '@mui/material/InputBase'
 import Badge from '@mui/material/Badge'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
-import SearchIcon from '@mui/icons-material/Search'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import NotificationsIcon from '@mui/icons-material/Notifications'
 import MoreIcon from '@mui/icons-material/MoreVert'
@@ -27,49 +24,12 @@ import {removeLoginToken} from '@/components/store/slices/auth/reducer'
 import {deleteCookie} from 'cookies-next'
 import {USER_TOKEN} from '@/components/utils/AppConfig'
 import {ToasterMessage} from '@/components/helpers/ToastMessage'
-
-const Search = styled('div')(({theme}) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}))
-
-const SearchIconWrapper = styled('div')(({theme}) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
-
-const StyledInputBase = styled(InputBase)(({theme}) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}))
+import Backdrop from '@mui/material/Backdrop'
+import SearchBox from './SearchBox'
 
 export default function AuthHeader(): React.JSX.Element {
   const dispatch = useAppDispatch()
+  const [openBackdrop, setOpenBackdrop] = React.useState(false)
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
     React.useState<null | HTMLElement>(null)
@@ -295,117 +255,124 @@ export default function AuthHeader(): React.JSX.Element {
       </MenuItem>
     </Menu>
   )
+  const handleCloseBackdrop = (): void => {
+    setOpenBackdrop(false)
+  }
+  const onFocusedInput = (): void => {
+    setOpenBackdrop(true)
+  }
 
   return (
-    <Box className="header">
-      <Box sx={{flexGrow: 1}} className="global_header">
-        <AppBar position="static">
-          <Toolbar>
-            <Image
-              src={logoLinkedin}
-              width={36}
-              height={36}
-              alt="linkedin-logo"
-            />{' '}
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon className="search_icon-header" />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{'aria-label': 'search'}}
+    <>
+      <Backdrop
+        sx={{color: '#fff', zIndex: 9999}}
+        open={openBackdrop}
+        onClick={handleCloseBackdrop}></Backdrop>
+      <Box className="header">
+        <Box sx={{flexGrow: 1}} className="global_header">
+          <AppBar position="static">
+            <Toolbar>
+              <Image
+                src={logoLinkedin}
+                width={36}
+                height={36}
+                alt="linkedin-logo"
+              />{' '}
+              <SearchBox
+                onFocusedInput={onFocusedInput}
+                openBackdrop={openBackdrop}
               />
-            </Search>
-            <Box sx={{flexGrow: 1}} />
-            <Box sx={{display: {xs: 'none', md: 'flex', gap: '13px'}}}>
-              <Link href="/">
+              <Box sx={{flexGrow: 1}} />
+              <Box sx={{display: {xs: 'none', md: 'flex', gap: '13px'}}}>
+                <Link href="/">
+                  <IconButton
+                    size="large"
+                    aria-label="show 4 new mails"
+                    className="header_icon"
+                    color="inherit">
+                    <Home />
+                    <Typography variant="h5">Home</Typography>
+                  </IconButton>
+                </Link>
                 <IconButton
                   size="large"
                   aria-label="show 4 new mails"
                   className="header_icon"
                   color="inherit">
-                  <Home />
-                  <Typography variant="h5">Home</Typography>
+                  <People />
+                  <Typography variant="h5">Network</Typography>
                 </IconButton>
-              </Link>
-              <IconButton
-                size="large"
-                aria-label="show 4 new mails"
-                className="header_icon"
-                color="inherit">
-                <People />
-                <Typography variant="h5">Network</Typography>
-              </IconButton>
 
-              <IconButton
-                size="large"
-                aria-label="show 4 new mails"
-                className="header_icon"
-                color="inherit">
-                <Work />
-                <Typography variant="h5">Jobs</Typography>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show 4 new mails"
-                className="header_icon"
-                color="inherit">
-                <Message />
-                <Typography variant="h5">Messaging</Typography>
-              </IconButton>
+                <IconButton
+                  size="large"
+                  aria-label="show 4 new mails"
+                  className="header_icon"
+                  color="inherit">
+                  <Work />
+                  <Typography variant="h5">Jobs</Typography>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  aria-label="show 4 new mails"
+                  className="header_icon"
+                  color="inherit">
+                  <Message />
+                  <Typography variant="h5">Messaging</Typography>
+                </IconButton>
 
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                className="header_icon"
-                color="inherit">
-                <Badge badgeContent={17} color="error">
-                  <NotificationsIcon />
-                </Badge>
-                <Typography variant="h5">Notifications</Typography>
-              </IconButton>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                className="header_icon"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit">
-                <AccountCircle />
-                <Typography variant="h5">Me</Typography>
-              </IconButton>
-            </Box>
-            <Box sx={{display: {xs: 'flex', md: 'none'}}}>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                className="header_icon"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit">
-                <AccountCircle />
-                <Typography variant="h5">Me</Typography>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                className="header_more_icon"
-                color="inherit">
-                <MoreIcon />
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
+                <IconButton
+                  size="large"
+                  aria-label="show 17 new notifications"
+                  className="header_icon"
+                  color="inherit">
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                  <Typography variant="h5">Notifications</Typography>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  className="header_icon"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit">
+                  <AccountCircle />
+                  <Typography variant="h5">Me</Typography>
+                </IconButton>
+              </Box>
+              <Box sx={{display: {xs: 'flex', md: 'none'}}}>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  className="header_icon"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit">
+                  <AccountCircle />
+                  <Typography variant="h5">Me</Typography>
+                </IconButton>
+                <IconButton
+                  size="large"
+                  aria-label="show more"
+                  aria-controls={mobileMenuId}
+                  aria-haspopup="true"
+                  onClick={handleMobileMenuOpen}
+                  className="header_more_icon"
+                  color="inherit">
+                  <MoreIcon />
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          {renderMobileMenu}
+          {renderMenu}
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
